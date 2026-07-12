739a93fb91fdd4d00395231deaa688eefa85c8e2
Moamen Hazem Abdelmoaty Zedan Saad <hazemzedan35@gmail.com>
2026-07-12 14:26:56 +0300

    Add files via upload

---
+++ week 4/figlet.py
@@ -0,0 +1,26 @@
+import pyfiglet
+
+import sys 
+
+import random
+
+# If no command line arguments, choose randomly a font
+font = pyfiglet.Figlet().getFonts()
+
+if len(sys.argv) ==1:
+    random_font = random.choice(font)
+    f = pyfiglet.Figlet(font=random_font)
+    print(f.renderText(input("input: ")))
+
+# If two command-line arguments, one to set the font, and other a valid font, set the font
+elif len(sys.argv) ==3 and sys.argv[1] in ("-f", "--font") and sys.argv[2] in (font):
+    f = pyfiglet.Figlet(font=sys.argv[2])
+    print(f.renderText(input("Input: ")))
+
+# If the user input wrong font or forget -f or -n the program will stop 
+else:
+    sys.exit("Invalid usage")
+
+
+
+
