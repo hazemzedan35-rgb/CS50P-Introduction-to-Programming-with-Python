bad29070d75b79c9c9e52b50bc846c16a832aad3
Moamen Hazem Abdelmoaty Zedan Saad <286130044+hazemzedan35-rgb@users.noreply.github.com>
2026-07-27 10:58:52 +0300

    Add files via upload

---
+++ week 6/scourgify.py
@@ -0,0 +1,41 @@
+import csv 
+
+import sys
+
+def main():
+    old, new = check_input(sys.argv)
+    split_write(old, new)
+
+
+
+def check_input(input):
+   
+    if len(input) <3:
+        sys.exit("too few command-line arguments")
+    elif len(input) > 3:
+        sys.exit("too many comman-line arguments")
+    old, new = input[1], input[2]
+    if old.endswith(".csv") and new.endswith(".csv"):
+        pass
+    else:
+        sys.exit("it is not csv file")
+
+    return old, new
+
+
+def split_write(old, new):
+    with open(old, "r") as file:
+        reader = csv.DictReader(file)
+
+        with open(new, "w", newline='') as f:
+            writer = csv.DictWriter(f, fieldnames= ["first", "last", "house"])
+            writer.writeheader()
+
+            for row in reader:
+                last, first = row["name"].split(", ")
+                writer.writerow({"first": first, "last": last, "house": row["house"]})
+
+
+
+if __name__=="__main__":
+    main()
